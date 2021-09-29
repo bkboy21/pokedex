d88ce34f71f15c0f97276ba757ac4991d04500a9
bkboy21 <89624517+bkboy21@users.noreply.github.com>
2021-09-29 14:27:33 -0400

    "Server is working and displays a plain index page

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const app = express();
+const Pokemon = require('./models/pokemon.js');
+const port = 3000;
+
+
+// INDEX
+app.get('/pokemon', (req, res) => {
+    res.render("index.ejs", { data: Pokemon});
+});
+
+
+// SHOW
+app.get('/pokemon/:id', (req, res) => {
+    res.render('show.ejs', { data: Pokemon[req.params.id] });
+});
+
+// NEW
+app.get('/pokemon/new', (req, res) => {
+    res.render('show.ejs', { data: Pokemon[req.params.id] });
+});
+
+// EDIT
+app.get('/pokemon/:id/edit', (req, res) => {
+    res.render('show.ejs', { data: Pokemon[req.params.id] });
+});
+
+
+// CREATE
+
+app.post('/pokemon', (req, res) => {
+    res.render('show.ejs', { data: Pokemon[req.params.id] });
+});
+
+
+// UPDATE
+
+app.put('/pokemon/:id', (req, res) => {
+    res.render('show.ejs', { data: Pokemon[req.params.id] });
+});
+
+
+// DESTROY
+app.delete('/pokemon/:id', (req, res) => {
+    res.render('show.ejs', { data: Pokemon[req.params.id] });
+});
+
+
+
+
+// Express Web Server port - app.listen
+app.listen(port, () => {
+    console.log(`listening on port`, port)
+});
